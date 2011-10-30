bddc99f145c64a46c7ddd921095a0bcb916020aa
Cgg <clement.geiger@gmail.com>
2011-10-30 14:06:24 +0100

    +PlopArc, PlopNode

---
+++ PlopArc.js
@@ -0,0 +1,33 @@
+PlopArc.prototype.STR_COLOR  = "rgba( 0, 0, 0, 1 )";
+
+PlopArc.prototype.LINE_WIDTH = 2;
+
+function PlopArc( nodeStart, nodeEnd )
+{
+  this.nodeStart = nodeStart;
+  this.nodeEnd   = nodeEnd;
+
+  this.click    = false;
+  this.hover    = false;
+  this.selected = false;
+}
+
+
+PlopArc.prototype.draw = function( canvasCtx )
+{
+  // TODO take into account click and hover
+
+  canvasCtx.save();
+
+  canvasCtx.strokeStyle = this.STR_COLOR;
+  canvasCtx.lineWidth   = this.LINE_WIDTH;
+
+  canvasCtx.beginPath();
+  canvasCtx.moveTo( this.nodeStart.x, this.nodeStart.y );
+  canvasCtx.lineTo( this.nodeEnd.x, this.nodeEnd.y );
+  canvasCtx.closePath();
+
+  canvasCtx.stroke();
+
+  canvasCtx.restore();
+}
---
+++ PlopNode.js
@@ -0,0 +1,94 @@
+PlopNode.prototype.TRANSITION = 125; // ms
+
+/* graphic constants */
+PlopNode.prototype.RADIUS     = 10; // px
+PlopNode.prototype.LINE_WIDTH = 2;
+
+PlopNode.prototype.STR_COLOR = "rgba( 0, 0, 0, 1 )";
+PlopNode.prototype.FIL_COLOR = "rgba( 255, 255, 255, 1 )";
+
+
+/* constructor */
+function PlopNode( x, y )
+{
+  this.x = x;
+  this.y = y;
+
+  //...
+
+  this.click    = false;
+  this.hover    = false;
+  this.selected = false;
+}
+
+
+PlopNode.SetHover = function( newHover )
+{
+  if( this.hover != newHover )
+  {
+    /*
+    if( newHover == true )
+    {
+      clickTr.Toggle();
+    }
+    else
+    {
+      clickTr.SetToZero();
+    }
+    */
+    this.hover = newHover;
+  }
+}
+
+PlopNode.SetClick = function( newClick )
+{
+  if( this.click != newClick )
+  {
+    /*
+    if( newClick == true )
+    {
+      clickTr.Toggle();
+    }
+    else
+    {
+      clickTr.SetToZero();
+    }
+    */
+    this.click = newClick;
+  }
+}
+
+
+PlopNode.prototype.SetPos = function( point )
+{
+  this.x = point.X;
+  this.y = point.Y;
+}
+
+/* Tells if the point P{X,Y} is in node or not */
+PlopNode.prototype.IsInNode = function( point )
+{
+  return( Math.sqrt( Math.pow( this.x - point.X, 2 ) +
+                     Math.pow( this.y - point.Y, 2 )   ) <= this.RADIUS );
+}
+
+/* Draw a node */
+PlopNode.prototype.draw = function( canvasCtx )
+{
+  // TODO take into account click and hover
+
+  canvasCtx.save();
+
+  canvasCtx.strokeStyle = this.STR_COLOR;
+  canvasCtx.fillStyle   = this.FIL_COLOR;
+  canvasCtx.lineWidth   = this.LINE_WIDTH;
+
+  canvasCtx.beginPath();
+  canvasCtx.arc( this.x, this.y, this.RADIUS, 0, Math.PI*2 );
+  canvasCtx.closePath();
+
+  canvasCtx.fill();
+  canvasCtx.stroke();
+
+  canvasCtx.restore();
+}
