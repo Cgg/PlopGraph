03159265b618d67406dddc97c7e816ad4b87f773
Cgg <clement.geiger@gmail.com>
2011-11-15 08:58:07 +0100

    +stubs for Point and Vector

--- Plop2DGeometry.js
+++ Plop2DGeometry.js
@@ -7,3 +7,66 @@
  *  - Vector
  *
  */
+
+/* class Point
+ *
+ * A Point instance is immutable
+ */
+
+function Point( X, Y )
+{
+  this.x = X;
+  this.y = Y;
+}
+
+
+/* class Vector
+ *
+ * A Vector instance is immutable, that is V1.Add( V2 ) wont modify V1 but
+ * return V3 = V1 + V3 instead.
+ */
+
+Vector.CreateFromPoints = function( P1, P2 )
+{
+  return new Vector( P2.x - P1.x, P2.y - P1.y );
+}
+
+function Vector( X, Y )
+{
+  this.x = X;
+  this.y = Y;
+}
+
+Vector.prototype.isNull = function()
+{
+  return ( this.x == 0 ) && ( this.y == 0 );
+}
+
+Vector.prototype.MultiplyByScalar = function( alpha )
+{
+  return new Vector( this.x * alpha, this.y * alpha );
+}
+
+Vector.prototype.Add = function( V )
+{
+  return new Vector( this.x + V.x, this.y + V.y );
+}
+
+Vector.prototype.IsColinearWith = function( V )
+{
+  if( !V.isNull() || this.isNull() )
+  {
+    var alphaX = V.x / this.x;
+    var alphaY = V.y / this.y;
+
+    return alphaX == alphaY;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+Vector.prototype.IsPointOnIt = function( P )
+{
+}
