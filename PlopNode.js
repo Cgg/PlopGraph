dbde5ea077b7c0eefe3d356902f1c7a53665917a
Cgg <clement.geiger@gmail.com>
2011-11-30 23:04:07 +0100

    +Node : update method

--- PlopNode.js
+++ PlopNode.js
@@ -21,7 +21,6 @@ function PlopNode( x, y, anchored )
   /* Physics properties */
   this.center       = new Point( x, y );
   this.velocity     = new Vector( 0, 0 );
-  this.acceleration = new Vector( 0, 0 );
 
   //...
 
@@ -31,6 +30,8 @@ function PlopNode( x, y, anchored )
   this.selected = false;
 
   this.arcs     = new Array( 0 );
+
+  setTimeout( PlopDelegate( this, this.update ), this.DT );
 }
 
 
@@ -52,18 +53,33 @@ PlopNode.prototype.IsInNode = function( point )
 };
 
 /* Update positions of a node, doing complex physics stuff */
-PlopNode.prototype.update = function( dt )
+PlopNode.prototype.update = function()
 {
   if( !( this.click || this.anchored ) )
   {
+    var dt = this.DT / 1000;
+
     // compute acceleration
- 
+    var forcesSum = new Vector( 0, 0 );
+
+    for( i = 0 ; i < arcs.length ; i++ )
+    {
+      forcesSum = forcesSum.Add( arcs[ i ].GetForceAppliedTo( this ) );
+    }
+
+    forcesSum = forcesSum.Add( 
+        this.velocity.MultiplyByScalar( -this.FRICTION ) );
+
+    var acc = forcesSum.MultiplyByScalar( 1 / this.MASS );
+
     // update velocity
+    this.velocity = this.velocity.Add( acc.MultiplyByScalar( dt ) );
 
     // update center's position
-
-    // this.center.
+    this.center.TranslateBy( this.velocity.MultiplyByScalar( dt ) );
   }
+
+  setTimeout( PlopDelegate( this, this.update ), this.DT );
 };
 
 /* Draw a node */
